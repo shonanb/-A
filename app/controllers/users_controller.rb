970a90dd308186f553b534c583a37b0ff4a80bcc
shonab <test@email.com>
2022-04-30 16:34:34 +0000

    test修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_or_correct_user, only: [:show, :edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
   before_action :set_one_month, only: :show
 
@@ -58,6 +58,15 @@ class UsersController < ApplicationController
     end
     redirect_to users_url
   end
+  
+  def admin_or_correct_user
+    @user = User.find(params[:id])
+    unless current_user?(@user) || current_user.admin?
+      flash[:danger]="権限がありません"
+      redirect_to(root_url)
+    end
+  end
+  
 
   private
 
